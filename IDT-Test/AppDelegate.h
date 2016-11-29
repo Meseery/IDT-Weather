dcaea9b06ce5cae9d68b62738ee6f9e363e19d33
Meseery <m.elmeseery@meunity.com>
2016-11-29 16:08:26 +0800

    Initial Commit

---
+++ IDT-Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IDT-Test
+//
+//  Created by Mohamed EL Meseery on 11/29/16.
+//  Copyright © 2016 Mohamed EL Meseery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IDT-Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IDT-Test
+//
+//  Created by Mohamed EL Meseery on 11/29/16.
+//  Copyright © 2016 Mohamed EL Meseery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
